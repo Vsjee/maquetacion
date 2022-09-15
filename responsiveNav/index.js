cdfc1177ea5b645f342d93e77ee074dcefb58b71
david hernandez <davidfelipehernandez1@gmail.com>
2022-09-14 21:40:29 -0500

    min

---
+++ responsiveNav/index.js
@@ -0,0 +1,8 @@
+const mainListDiv = document.getElementById("mainListDiv")
+const mediaButton = document.getElementById("mediaButton")
+
+mediaButton.onclick = function () {
+  "use strict";
+  mainListDiv.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
+};
